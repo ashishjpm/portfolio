55c6871352e64ad005bcd2a7ebf0fce5962917c1
ashishjpm <ashish.mishra@nanobianalytics.com>
2016-06-22 18:05:20 +0530

    decouple html, click action on headerTabs recorded

--- app/scripts/controllers/main.ctrl.js
+++ app/scripts/controllers/main.ctrl.js
@@ -1,11 +1,24 @@
 /**
  * @ngdoc function
- * @name portfolio.controller:MainCtrl
+ * @name portfolio.controller:IndexCtrl
  * @description
- * # MainCtrl
+ * # IndexCtrl
  * @author Ashish Mishra <ashishjpm@gmail.com>
  * Controller of the portfolio
  */
 angular.module('portfolio.controllers')
 .controller('IndexCtrl', ['$scope', function ($scope) {
+	//Global var
+
+	function initCtrl(){
+		//Predefind $scope
+		$scope.headerTabs = ['about me','resume','contact','resources'];
+	}
+
+	$scope.headerTabClick = function(index,clickedElement){
+		console.log(index,clickedElement);
+	}
+
+	initCtrl();
+
   }]);
